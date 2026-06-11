762a1f1ef8a54a26a76e1291ba35d323c8dd993b
Rafaela Pereira <pereira.r@esudante.ifro.edu.br>
2026-06-11 17:30:11 -0400

    Remove node_modules do controle do Git

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('inicial');
 });
 
 app.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
+});
+
+app.get('/login', (req,res)=>{
+    res.render('autenticacao/login');
+});
+
+app.get('/produtos', (req,res)=>{
+    res.render('produto/listar');
 });
